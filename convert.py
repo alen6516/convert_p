4a9251574fadd17f6a0b9a14e33b9a0334d12774
alen Liou <alen6516@gmail.com>
2017-10-14 15:47:44 +0800

    third commit

--- convert.py
+++ convert.py
@@ -20,19 +20,21 @@ if __name__=='__main__':
     print("*this module convert file encoding from gb2312 to utf-8*")
     print("********************************************************")
     if len(sys.argv) <2:
-        file_name=raw_input("give the file name:")
+        file=raw_input("give the file name:")
     else:
-        file_name = sys.argv[1]
-    r=open(file_name)
-    result='result_'+file_name
-    w=open(result, 'w')
+        file = sys.argv[1:]
+    for file_name in file:
+        r=open(file_name)
+        result='result_'+file_name
+        w=open(result, 'w')
 
-    
-    print("input file_name = %s" % file_name)
-    print("output file_name = %s" % result)
-    print("converting ...")
+        
+        print("input file_name = %s" % file_name)
+        print("output file_name = %s" % result)
+        print("converting ...")
 
-    for line in r:
-        w.write(S2T(line.decode('GB2312', 'replace').encode('utf-8')))
-        #w.write(S2T(line))
+        for line in r:
+            w.write(S2T(line.decode('GB2312', 'replace').encode('utf-8')))
+            #w.write(S2T(line))
+        print("%s done" % file_name)
     print("conversion done")
