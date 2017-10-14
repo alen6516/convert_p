92eff17dc09d38797b4f3ad68f6b16db08c2853a
alen Liou <alen6516@gmail.com>
2017-10-14 16:08:21 +0800

    4th commit

--- convert.py
+++ convert.py
@@ -16,9 +16,9 @@ def T2S(line):
     return line.encode('utf-8')
 
 if __name__=='__main__':
-    print("********************************************************")
-    print("*this module convert file encoding from gb2312 to utf-8*")
-    print("********************************************************")
+    print("***********************************************************")
+    print("* this module decodes file in gb2312 and encodes to utf-8 *")
+    print("***********************************************************")
     if len(sys.argv) <2:
         file=raw_input("give the file name:")
     else:
