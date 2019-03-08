c7d04d8b057a6d3d17763cecd5063904d33e57f0
alen6516 <alen6516@gmail.com>
2019-03-08 23:03:29 +0800

    correct convert.py

--- convert.py
+++ convert.py
@@ -7,34 +7,31 @@ import sys
 import chardet
 from langconv import *
 
-def S2T(line):
-    line = Converter('zh-hant').convert(line.decode('utf-8'))
-    return line.encode('utf-8')
+def S2T(string):
+    return Converter('zh-hant').convert(string)
+    
 
-def T2S(line):
-    line = Converter('zh-hans').convert(line.decode('utf-8'))
-    return line.encode('utf-8')
+def T2S(string):
+    return Converter('zh-hans').convert(string)
 
 if __name__=='__main__':
     print("***********************************************************")
-    print("* this module decodes file in gb2312 and encodes to utf-8 *")
+    print("* this module decodes file in gb and encodes to utf-8 *")
     print("***********************************************************")
     if len(sys.argv) <2:
         file=raw_input("give the file name:")
     else:
         file = sys.argv[1:]
     for file_name in file:
-        r=open(file_name)
+        fo=open(file_name, "r", encoding="gbk", errors="replace")
         result='result_'+file_name
-        w=open(result, 'w')
-
+        fw=open(result, 'w')
         
         print("input file_name = %s" % file_name)
         print("output file_name = %s" % result)
         print("converting ...")
 
-        for line in r:
-            w.write(S2T(line.decode('GB2312', 'replace').encode('utf-8')))
-            #w.write(S2T(line))
+        for line in fo.readlines():
+            fw.write(S2T(line))
         print("%s done" % file_name)
     print("conversion done")
