8c7b3cef79de3ec0effc54cb93aa9b858c8eee86
alen6516 <alen6516@gmail.com>
2019-03-13 12:51:33 +0800

    update convert.py

--- convert.py
+++ convert.py
@@ -14,6 +14,15 @@ def S2T(string):
 def T2S(string):
     return Converter('zh-hans').convert(string)
 
+
+def if_encode_by_GB2312(lines):
+    count = 0
+    for i in range(10):
+        if chardet.detect(lines[i])['encoding'] == 'GB2312':
+            count += 1
+    
+    return True if count >=5 else False
+
 if __name__=='__main__':
     print("***********************************************************")
     print("* this module decodes file in gb and encodes to utf-8 *")
@@ -23,15 +32,26 @@ if __name__=='__main__':
     else:
         file = sys.argv[1:]
     for file_name in file:
-        fo=open(file_name, "r", encoding="gbk", errors="replace")
+
+        fo=open(file_name, 'rb')
+        lines = fo.readlines()
+        fo.close()
+
+        if if_encode_by_GB2312(lines):
+            print("%s is encode by GB2312" % file_name)
+        else:
+            print("%s is not encode by GB2312, ignore" % file_name)
+            continue
+
         result='result_'+file_name
-        fw=open(result, 'w')
         
         print("input file_name = %s" % file_name)
         print("output file_name = %s" % result)
         print("converting ...")
 
-        for line in fo.readlines():
-            fw.write(S2T(line))
+        fw=open(result, 'w')
+        for line in lines:
+            fw.write(S2T(line.decode('gbk')))
+        fw.close()
         print("%s done" % file_name)
     print("conversion done")
